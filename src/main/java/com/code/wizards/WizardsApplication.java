3693f05a763054c113688ed12be5beed981ed7b3
shrionit <sptiwari46289@gmail.com>
2022-08-26 16:38:56 +0530

    init

---
+++ src/main/java/com/code/wizards/WizardsApplication.java
@@ -0,0 +1,13 @@
+package com.code.wizards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardsApplication.class, args);
+	}
+
+}
